41b40610c83ca645f0a95e9517b02c86e7e9a2a9
Su-u-un <tl071515@gmail.com>
2023-11-22 09:54:09 +0800

    [feat] 重新写了前端逻辑。准备写图片验证码，作此保存

--- server/controllers/UsersControllers.js
+++ server/controllers/UsersControllers.js
@@ -5,7 +5,7 @@ exports.login = async (req,res)=>{
     const data = req.body
     if(!data.username||!data.password){
         res.send({
-            code: "-1",//返回信息
+            code: "-2",//返回信息
             error: "用户名密码不能为空"
         })
         return
@@ -15,7 +15,7 @@ exports.login = async (req,res)=>{
         // 登录校验
         if (result.length === 0) {
             res.send({
-                code: "-1",//返回信息
+                code: "-2",//返回信息
                 error: "用户名密码不匹配"
             })
         } else {
@@ -23,7 +23,7 @@ exports.login = async (req,res)=>{
             const token = JWT.generate({
                 _id: result[0]._id,
                 username: result[0].username
-            }, "7d")
+            }, "5d")
             res.header("Authorization",token)//同时将token在响应头中返回给前端
             res.send({
                 code: "0",//返回信息，
@@ -39,7 +39,7 @@ exports.register = async (req,res)=>{
     // 处理发来的参数，防止数据库操作报错
     if(!data.username||!data.password){
         res.send({
-            code: "-1",//返回信息
+            code: "-2",//返回信息
             error: "用户名密码不能为空"
         })
         return
@@ -53,7 +53,7 @@ exports.register = async (req,res)=>{
             .then(result=>{
                 if(result.affectedRows !== 1){
                     res.send({
-                        code: "-1",//返回信息
+                        code: "-2",//返回信息
                         error: "注册用户失败，请联系管理员"
                     })
                 }else{
@@ -65,20 +65,66 @@ exports.register = async (req,res)=>{
                 }
             }).catch(err => {
                 res.send({
-                    code: '-1',
+                    code: '-2',
                     error: err
                 })
               })
         }else{
             res.send({
-                code: "-1",
+                code: "-2",
                 error: "用户名已存在"
             })
         }
     }).catch(err => {
         res.send({
-            code: '-1',
+            code: '-2',
             error: err
         })
       })
+}
+
+exports.updatePWD = async (req,res)=>{
+    const data = req.body
+    await usersDB.updatePWD([data.password,data.username])
+    .then(result=>{
+        if(result.affectedRows !== 1){
+            res.send({
+                code: "-2",//返回信息
+                error: "修改密码失败，请联系管理员"
+            })
+        }else{
+            res.send({
+                ActionType: "OK",
+                code: "0",//返回信息，
+                msg:"success"
+            })
+        }
+    }).catch(err => {
+        res.send({
+            code: '-2',
+            error: err
+        })
+    })
+}
+
+exports.info = async (req,res)=>{
+    const token = req.headers["authorization"].split(" ")[1]
+    const payload = JWT.verify(token)
+    const username = payload.username
+    await usersDB.info(username).then(result=>{
+        res.send({
+            ActionType: "OK",
+            code: "0",//返回信息，
+            data:result[0]
+        })
+    }).catch(err => {
+        res.send({
+            code: '-2',
+            error: err
+        })
+    })
+}
+
+exports.getCaptcha = async (req,res)=>{
+    
 }
